970028e37af5ffb456af336b6baa816c4daf2ae6
Maniru <manirkhalil@gmail.com>
2025-03-21 06:45:30 +0000

    Added basic Flask app with home route

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Maths Exam Prep!"
 
 if __name__ == '__main__':
     app.run(debug=True)
