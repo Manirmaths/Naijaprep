2bccbaa26c7ce231db125b7d6568c1f0663f49f9
Maniru <manirkhalil@gmail.com>
2025-03-23 07:03:36 +0000

    ENV

--- app/__init__.py
+++ app/__init__.py
@@ -3,28 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail  # Add Flask-Mail
+from flask_mail import Mail
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file (for local development)
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'maniru-samira-khalil-282805'  # Ensure this is a strong, unique key
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///site.db').replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Flask-Mail configuration (example with Gmail)
+# Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'manirkhalil@gmail.com'  # Replace with your Gmail address
-app.config['MAIL_PASSWORD'] = 'kozo kbsx pmkm vmzg'  # Replace with Gmail App Password
-app.config['MAIL_DEFAULT_SENDER'] = 'manirkhalil@gmail.com'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
 
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 bcrypt = Bcrypt(app)
-mail = Mail(app)  # Initialize Flask-Mail
+mail = Mail(app)
 
 # Import models to register them with SQLAlchemy
 from app.models import User, Question, UserResponse
